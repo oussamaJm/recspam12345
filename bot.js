af8cb013f5c9f50394cf32adce75f1e2b61e5c22
oussamaJm <49239175+oussamaJm@users.noreply.github.com>
2019-07-19 22:04:46 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("601854081648164895")
+setInterval(function() {
+channel.send(`اكتب هنا ااش تريد يكتب فب السبام`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
